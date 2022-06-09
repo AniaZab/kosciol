5f1f6473ad81190b812e8dbe55dac6604cd24dc1
AniaZab <79911632+AniaZab@users.noreply.github.com>
2022-06-09 09:59:33 +0200

    dodanie kodów js i css

--- src/main/resources/static/main/javaScript.js
+++ src/main/resources/static/main/javaScript.js
@@ -1,3 +1,10 @@
-function showAlert() {
-    alert("The button was clicked!");
+const pagesInWebsite = ["PageRegularEvents", "PageNewEvents", "PageRegister", "PageLogin", "PageAddEvent"];
+
+function show(shown) {
+    pagesInWebsite.forEach(page => {
+        document.getElementById(page).style.display = 'none'
+    })
+    document.getElementById(shown).style.display = 'block';
+    return false;
 }
+show("PageRegularEvents");
