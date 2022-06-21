d17aec2373de90efd15e9fd9fd8000ea15fd5f60
AniaZab <79911632+AniaZab@users.noreply.github.com>
2022-06-21 10:57:24 +0200

    zmiana nazwy funkcji

--- src/main/resources/static/main/javaScript.js
+++ src/main/resources/static/main/javaScript.js
@@ -1,13 +1,10 @@
 const pagesInWebsite = ["PageRegularEvents", "PageNewEvents", "PageRegister", "PageLogin", "PageAddEvent"];
 
-function show(shown) {
+function showPage(shown) {
     pagesInWebsite.forEach(page => {
         document.getElementById(page).style.display = 'none'
     })
     document.getElementById(shown).style.display = 'block';
     return false;
 }
-function changeLanguage(str) {
-    document.getElementById('language').value = str.value;
-}
-show("PageRegularEvents");
+showPage("PageRegularEvents");
