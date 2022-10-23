2e8016701a63bbb108a38584ed53bd1ada959ebf
AniaZab <79911632+AniaZab@users.noreply.github.com>
2022-10-23 17:36:54 +0200

    wstepne html

---
+++ src/main/resources/static/userPage/javaScript.js
@@ -0,0 +1,32 @@
+const pagesInWebsite = ["PageEvents", "PageChangeUserData", "PageAddUser"];
+
+function showPage(shown) {
+    try {
+        pagesInWebsite.forEach(page => {
+            document.getElementById(page).style.display = 'none'
+        })
+        document.getElementById(shown).style.display = 'block';
+    }
+    catch (error){
+        console.error(error);
+    }
+    return false;
+}
+
+var serverContext = "[[@{/}]]"
+    function savePass(){
+        var pass = $("#pass").val();
+        var valid = pass == $("#passConfirm").val();
+        if(!valid) {
+            $("#error").show();
+            return;
+        }
+        $.post(serverContext + "user/updatePassword",
+            {password: pass, oldpassword: $("#oldpass").val()} ,function(data){
+                window.location.href = serverContext +"/home.html?message="+data.message;
+            })
+            .fail(function(data) {
+                $("#errormsg").show().html(data.responseJSON.message);
+            });
+    }
+showPage("PageEvents");
