510d39b84b91b12e2c2bd0b36e88c9a217fd7b70
AniaZab <79911632+AniaZab@users.noreply.github.com>
2022-11-19 21:20:01 +0100

    dodanie f. existsEvent

--- src/main/java/com/apka/kosciol/repository/IUser.java
+++ src/main/java/com/apka/kosciol/repository/IUser.java
@@ -1,6 +1,7 @@
 package com.apka.kosciol.repository;
 
 import com.apka.kosciol.entity.User;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,9 @@ public interface IUser extends JpaRepository<User, Integer> {
    // @Query("SELECT * FROM User_Table WHERE email = email_adress")
     Optional<User> findByEmail(@Param("email") String email);
     Optional<User> findByLogin(@Param("login") String login);
-//    //List<User> search(String stringFilter);
+    boolean existsEventByEmail(@Param("email") String email);
+    boolean existsEventByLogin(@Param("login") String login);
+    //    //List<User> search(String stringFilter);
 /*    @Query("select * from [church].[dbo].[User_Table] " +
             "where lower(c.firstName) like lower(concat('%', :searchTerm, '%')) " +
             "or lower(c.lastName) like lower(concat('%', :searchTerm, '%'))")
