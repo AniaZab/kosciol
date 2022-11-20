d45d0b5910d249e17f1c51cb8cb33bebe03d77a2
AniaZab <79911632+AniaZab@users.noreply.github.com>
2022-11-20 20:52:24 +0100

    dodanie wart. do enumów

--- src/main/java/com/apka/kosciol/entity/RecipientCategory.java
+++ src/main/java/com/apka/kosciol/entity/RecipientCategory.java
@@ -1,13 +1,23 @@
 package com.apka.kosciol.entity;
 
 public enum RecipientCategory {
-    MŁODZIEŻ,
-    DZIECI,
-    DOROŚLI,
-    WSZYSCY,
-    LIDERZY,
-    MAŁŻEŃSTWA,
-    RODZINY,
-    SINGLE,
-    INNE
+    MŁODZIEŻ("Młodzież"),
+    DZIECI("Dzieci"),
+    DOROŚLI("Dorośli"),
+    WSZYSCY("Wszyscy"),
+    LIDERZY("Liderzy"),
+    MAŁŻEŃSTWA("Małżeństwa"),
+    RODZINY("Rodziny"),
+    SINGLE("Single"),
+    INNE("Inne");
+
+    private final String displayValue;
+
+    private RecipientCategory(String displayValue) {
+        this.displayValue = displayValue;
+    }
+
+    public String getDisplayValue() {
+        return displayValue;
+    }
 }
