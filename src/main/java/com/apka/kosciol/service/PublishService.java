823182104740bf788e4fe55c8aa6332c39c17f4e
AniaZab <79911632+AniaZab@users.noreply.github.com>
2022-12-27 16:51:11 +0100

    wstepny publish

--- src/main/java/com/apka/kosciol/service/PublishService.java
+++ src/main/java/com/apka/kosciol/service/PublishService.java
@@ -1,17 +1,34 @@
 package com.apka.kosciol.service;
 
+import com.apka.kosciol.config.MailConfiguration;
 import com.apka.kosciol.dto.EventDto;
 import com.apka.kosciol.dto.RecipientDto;
 import com.apka.kosciol.dto.UserDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.mail.*;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 import java.util.List;
+import java.util.Properties;
 
 @Service
 @RequiredArgsConstructor
 public class PublishService {
-    EventDto eventToSend; List<RecipientDto> recipientDtoList; UserDto sender;
+
+    private EventDto eventToSend;
+    private List<RecipientDto> recipientDtoList;
+    private UserDto sender;
+
+    public PublishService(EventDto eventToSend, List<RecipientDto> recipientDtoList, UserDto sender) {
+        this.eventToSend = eventToSend;
+        this.recipientDtoList = recipientDtoList;
+        this.sender = sender;
+    }
+
     //EventDto eventToSend, List<RecipientDto> recipientDtoList, UserDto sender
     public void publish(){
         if(eventToSend.getEmailPublish()){
@@ -25,11 +42,49 @@ public class PublishService {
         }
     }
 
-    private void prepareEmail(){}
+    private void publishEmail(){
+        String from = sender.getEmail();
+        Authenticator auth = new Email_Autherticator();
 
-    private void publishEmail(){}
+        //prop.put("mail.smtp.host", "smtp.mailtrap.io");
+        //prop.put("mail.smtp.ssl.trust", "smtp.mailtrap.io");
+
+       // prop.setProperty("mail.smtp.host", host);
+
+        Session session = Session.getInstance(MailConfiguration.getConfiguration(), auth);
+        /*Session session = Session.getDefaultInstance(prop,
+                new Authenticator(){
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication("username", "password");
+                    }});*/
+        System.out.println("auth");
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(from));
+            for (RecipientDto recipientDto:
+                    recipientDtoList) {
+                message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipientDto.getEmail()));
+            }
+            //message.setHeader();
+            message.setSubject(eventToSend.getTitle());
+
+            // Now set the actual message
+            message.setText(eventToSend.getDescription(), "text/html; charset=utf-8");
+
+            // Send message
+            Transport.send(message);
+            System.out.println("Sent message successfully....");
+        } catch (MessagingException mex) {
+            mex.printStackTrace();
+        }
+        //prepareEmail();
+    }
 
     private void publishFacebook(){}
 
     private void publishMessenger(){}
+
+    private void prepareEmail(){
+
+    }
 }
