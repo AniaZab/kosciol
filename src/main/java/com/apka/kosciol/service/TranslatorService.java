273014ca9129081cf2bc6faaeac7d93f56c4ec89
AniaZab <79911632+AniaZab@users.noreply.github.com>
2023-01-03 15:38:56 +0100

    usuniecie niepotrzebnych rzeczy

--- src/main/java/com/apka/kosciol/service/TranslatorService.java
+++ src/main/java/com/apka/kosciol/service/TranslatorService.java
@@ -2,8 +2,6 @@ package com.apka.kosciol.service;
 
 import com.apka.kosciol.config.LocaleConfiguration;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Service;
 
@@ -21,8 +19,7 @@ public class TranslatorService {
     }
 
     public String[] toLocale(String[] code) {
-       Locale locale=Locale.getDefault();
-        //System.out.println("mza " + locale);
+        Locale locale = Locale.getDefault();
         String[] codeResult = new String[code.length];
         for (int i = 0; i < codeResult.length; i++) {
             codeResult[i] = messageSource.getMessage(code[i], null, locale);
