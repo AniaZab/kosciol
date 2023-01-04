f5699c7a9c556b1fcfd59341e11b6dc441ea4142
AniaZab <79911632+AniaZab@users.noreply.github.com>
2023-01-04 09:30:12 +0100

    usuniecie translatorService

--- src/main/java/com/apka/kosciol/service/TranslationService.java
+++ src/main/java/com/apka/kosciol/service/TranslationService.java
@@ -1,16 +1,28 @@
 package com.apka.kosciol.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Service;
+
+import java.util.Locale;
 
 import static com.apka.kosciol.util.TranslationCode.names;
 
 @Service
 @RequiredArgsConstructor
 public class TranslationService {
-    private final TranslatorService translatorService;
+    private final ResourceBundleMessageSource messages;
 
     public String[] getTranslation() {
-        return translatorService.toLocale(names);
+        return toLocale(names);
+    }
+
+    public String[] toLocale(String[] code) {
+        Locale locale = Locale.getDefault();
+        String[] codeResult = new String[code.length];
+        for (int i = 0; i < codeResult.length; i++) {
+            codeResult[i] = messages.getMessage(code[i], null, locale);
+        }
+        return codeResult;
     }
 }
